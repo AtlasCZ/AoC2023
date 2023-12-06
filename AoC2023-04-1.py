ec9ab6fb75d950df283b17b662a4e6bd57e4827b
AtlasCZ <92540144+AtlasCZ@users.noreply.github.com>
2023-12-06 11:29:33 +0100

    Add files via upload

--- AoC2023-04-1.py
+++ AoC2023-04-1.py
@@ -1,5 +1,3 @@
 s = [input() for _ in range(206)]
-y = 0
-for x in s:
-    y += int(2 ** (len(set(x.replace(x[0:x.index(":") + 2], "").split("|")[0].split()).intersection(set(x.replace(x[0:x.index(":") + 2], "").split("|")[1].split()))) - 1))
-print(y)
+print(sum(int(2 ** (len(set(x.replace(x[0:x.index(":") + 2], "").split("|")[0].split()).intersection(set(x.replace(x[0:x.index(":") + 2], "").split("|")[1].split()))) - 1)) for x in s))
+
