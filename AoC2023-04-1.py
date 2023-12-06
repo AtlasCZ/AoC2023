38be50f421753259d1df72e2544791b2445622e7
AtlasCZ <92540144+AtlasCZ@users.noreply.github.com>
2023-12-06 11:30:28 +0100

    Add files via upload

--- AoC2023-04-1.py
+++ AoC2023-04-1.py
@@ -1,3 +1,2 @@
 s = [input() for _ in range(206)]
 print(sum(int(2 ** (len(set(x.replace(x[0:x.index(":") + 2], "").split("|")[0].split()).intersection(set(x.replace(x[0:x.index(":") + 2], "").split("|")[1].split()))) - 1)) for x in s))
-
