8433398621fbebc4e6085f060e638ce9021efb31
AtlasCZ <92540144+AtlasCZ@users.noreply.github.com>
2023-12-06 11:32:26 +0100

    Add files via upload

--- AoC2023-04-1.py
+++ AoC2023-04-1.py
@@ -1,2 +1,1 @@
-s = [input() for _ in range(206)]
-print(sum(int(2 ** (len(set(x.replace(x[0:x.index(":") + 2], "").split("|")[0].split()).intersection(set(x.replace(x[0:x.index(":") + 2], "").split("|")[1].split()))) - 1)) for x in s))
+print(sum(int(2 ** (len(set(x.replace(x[0:x.index(":") + 2], "").split("|")[0].split()).intersection(set(x.replace(x[0:x.index(":") + 2], "").split("|")[1].split()))) - 1)) for x in [input() for _ in range(206)]))
